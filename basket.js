c2e483c2fde9b80a58835f25b1446930fc15d356
Viktorya Evsina <viktoryews1997@mail.ru>
2023-12-27 18:58:11 +0300

    рабочий вариант

---
+++ basket.js
@@ -0,0 +1,25 @@
+async function productData() {
+    try {
+        const resp = await fetch('data.json');
+        if (!resp.ok) {
+            throw new Error("Получена ошибка при обработке data.json");
+        }
+        const data = await resp.json();
+        const containerBox = document.querySelector('.container');
+        data.forEach(({ photo, product_name, product_description, product_price }) => {
+            const productCard = `
+            <div class="card">
+             <img class="product_img" src="${photo}" alt="${product_name}">
+             <h2 class="product_name">${product_name}</h2>
+             <p class="product_description">${product_description}</p>
+             <p class="product_price">$${product_price}</p>
+            </div>   
+        `
+            containerBox.insertAdjacentHTML("beforeend", productCard);
+        });
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+productData();
